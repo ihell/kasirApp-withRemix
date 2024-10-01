38abd36ce6085c307fc1c46d5cbdb55b07a082ec
ihell <ihell269@gmail.com>
2024-10-01 20:01:41 +0700

    push

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+
+
 interface Product {
   id: number;
   name: string;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Path ke folder Remix
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
